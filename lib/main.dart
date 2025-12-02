d719efba7d2ffc9ee4643839bef402dd2147b66b
Killian Lacaque <killian@MacBook-Air-de-Killian.local>
2025-12-02 18:38:07 +0100

    Ajout de fonctionnalités locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
 // lib/main.dart
+import 'package:culturek/data/services/data_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'config/firebase_options.dart';
 import 'ui/screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Initialisation directe pour le Web
+  // --- NOUVELLE LOGIQUE D'INITIALISATION ---
+  
+  // 2. Charger les variables d'environnement depuis le fichier .env
+  await dotenv.load(fileName: ".env");
+
+  // 3. Initialiser Firebase manuellement avec ces variables
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
+    ),
   );
   
-  runApp(const MyApp());
+  // On ne charge plus les données ici, le SplashScreen le fait.
+  // await DataManager.instance.loadAllData();
+  
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => DataManager.instance,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
