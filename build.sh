eda18a1e8916c7672a72bd20b051d3000f580bc2
Killian Lacaque <killian@MacBook-Air-de-Killian.local>
2025-12-02 18:00:43 +0100

    Ajout script build vercel

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# On lance la construction avec toutes les clés injectées depuis Vercel
+# 1. On ajoute le dossier Flutter au "Chemin" pour que le script le trouve
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# 2. (Optionnel) On vérifie que ça marche dans les logs
+echo "Chemin Flutter : $(which flutter)"
+echo "Version Flutter :"
+flutter --version
+
+# 3. On lance la construction avec les clés
 flutter build web --release \
   --dart-define=FIREBASE_API_KEY="$FIREBASE_API_KEY" \
   --dart-define=FIREBASE_APP_ID="$FIREBASE_APP_ID" \
